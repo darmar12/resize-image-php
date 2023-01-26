9cdd3dccf684d2b177cd614f867187b8b515dc38
Dima Ovharenko <dima-o@mail.ru>
2023-01-26 19:29:34 +0300

    remove comments

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
-// $imagine->open('cat.jpg')
-//     ->thumbnail(new \Imagine\Image\Box(500, 500))
-//     ->save("resize_cat.jpg");
-
 class resizeImage {
     public $imagine;
     public function __construct() {
