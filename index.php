c504e6561bb691616a2bbf5edd52ac52dd704218
Dima Ovharenko <dima-o@mail.ru>
2023-01-26 15:07:48 +0300

    try solo image resize

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
 <?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+$imagine = new \Imagine\Gd\Imagine();
+$imagine->open('cat.jpg')
+    ->thumbnail(new \Imagine\Image\Box(500, 500))
+    ->save("resize_cat.jpg");
