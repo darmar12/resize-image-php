50b186dc47553a5dcc2280d7e7e5d502bde69322
Dima Ovharenko <dima-o@mail.ru>
2023-01-26 19:22:21 +0300

    class resizeImage

--- index.php
+++ index.php
@@ -1,7 +1,30 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
-$imagine = new \Imagine\Gd\Imagine();
-$imagine->open('cat.jpg')
-    ->thumbnail(new \Imagine\Image\Box(500, 500))
-    ->save("resize_cat.jpg");
+// $imagine->open('cat.jpg')
+//     ->thumbnail(new \Imagine\Image\Box(500, 500))
+//     ->save("resize_cat.jpg");
+
+class resizeImage {
+    public $imagine;
+    public function __construct() {
+        $this->imagine = new \Imagine\Gd\Imagine();
+    }
+
+    public function resizeAll($dir) {
+        $arrExts = ["jpg", "jpeg", "png", "webp"];
+        $files = scandir($dir);
+        foreach($files as $key => $value) {
+            $path = realpath($dir . '/' . $value);
+            $ext = pathinfo($path, PATHINFO_EXTENSION);
+            if(in_array($ext, $arrExts)) {
+                $this->imagine->open($path)
+                              ->thumbnail(new \Imagine\Image\Box(500, 500))
+                              ->save($path);
+            }
+        }
+    }
+}
+
+$resizer = new resizeImage();
+$resizer->resizeAll('./images');
